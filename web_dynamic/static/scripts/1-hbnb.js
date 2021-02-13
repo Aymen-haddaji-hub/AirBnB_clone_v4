016e76c53632dccf0c26d2a05fe3459db9de1cb8
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-13 18:29:36 +0100

    fix semistandard

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,11 @@
-const $ = window.$;
-window.addEventListener('DOMContentLoaded', (event) => {
-  const amenityDict = {};
-  $('input[type=checkbox]').change(function () {
-    if (this.checked) {
-      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+$('document').ready(function () {
+  const amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      delete amenityDict[$(this).attr('data-id')];
+      delete amenities[$(this).attr('data-id')];
     }
-    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+    $('.amenities H4').text(Object.values(amenities).join(', '));
   });
 });
